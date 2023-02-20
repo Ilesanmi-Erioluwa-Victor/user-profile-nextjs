19f90ba9aac49d792ba2fe4751e5cd8112c0c701
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-20 07:42:55 +0100

    Working on user Experience

--- src/components/Content/Experience.tsx
+++ src/components/Content/Experience.tsx
@@ -6,7 +6,12 @@ const Experience= ():JSX.Element => {
       <div>
         <h2>Experience</h2>
         <p>I specialized in UX/UI design, brand strategy, and Web flow Development.</p>
+        <hr className='bg-orange-600 w-full h-1' />
+        <div>
+          <h2>About me</h2>
+        </div>
       </div>
+
        
       {/* Skills */}
       <div>
